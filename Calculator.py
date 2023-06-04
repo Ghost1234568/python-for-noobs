6f710ceb027a69d410c5edf9c5dfb9944beb64f5
Ghost1234568 <135494446+Ghost1234568@users.noreply.github.com>
2023-06-04 12:45:04 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -2,20 +2,12 @@
 #return is a function which gives back the output
 def add(x, y):
     return (x + y)
-
-
 def sub(x, y):
     return (x - y)
-
-
 def multi(x, y):
     return (x * y)
-
-
 def div(x, y):
     return (x / y)
-
-
 def power(x, y):
     return (x ** y)
 
@@ -26,17 +18,17 @@ print("""addition\nsubtraction\nmultiplication\ndivision\nexponent\n""")
 x = int(input("Enter the number\n"))
 y = int(input("Enter the number\n"))
 
-while (True):
-    a = input("Enter what to do\n")
-    if a == "addition":
-        add(x, y)
-    elif a == "subtraction":
-        sub(x, y)
-    elif a == "multiplication":
-        multi(a, y)
-    elif a == "division":
-        div(x, y)
-    elif a == "exponent":
-        power(x, y)
-    else:
-        break
+a = input("Enter what to do\n")
+
+if a == "addition":
+    add(x, y)
+elif a == "subtraction":
+    sub(x, y)
+elif a == "multiplication":
+    multi(a, y)
+elif a == "division":
+    div(x, y)
+elif a == "exponent":
+    power(x, y)
+else:
+    print("Invalid command")
