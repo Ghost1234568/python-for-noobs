4938ccaf8ec3e549a604fdaa46e8a032b2c71ced
Ghost1234568 <135494446+Ghost1234568@users.noreply.github.com>
2023-06-04 22:10:09 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,5 +1,5 @@
-#def() is a function which allows us define certain value inside it
-#print is a function which gives back the output
+# "def()" is a function which allows us define certain value inside it
+# "print" is a function which gives back the output
 def add(x, y):
    print(x + y)
 def sub(x, y):
@@ -13,8 +13,8 @@ def power(x, y):
    
 print("""addition\nsubtraction\nmultiplication\ndivision\nexponent\n""") #print function display what we entered in the "" or ''
 
-x = int(input("Enter the number\n"))#Here x is a variable where i can give a value to it and int is called integer similarly we have float, bool, complex, str ect..
-y = int(input("Enter the number\n"))#input function allow's us to enter our own value
+x = int(input("Enter the number\n"))# Here x is a variable where i can give a value to it and int is called integer similarly we have float, bool, complex, str ect..
+y = int(input("Enter the number\n"))# "input" function allow's us to enter our own value
 a = input("Enter what to do\n")
 
 if a == "addition":# "if" is an condition statment followed by "elif" and "else"
@@ -29,3 +29,4 @@ elif a == "exponent":
     power(x, y)
 else:
     print("Invalid command")
+# Follow me for python content
